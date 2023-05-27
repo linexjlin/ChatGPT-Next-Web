4bbf54cc2f8b530729f39a45b1b5c4750f149546
linexjlin <line.xj.lin@gmail.com>
2023-05-27 15:49:53 +0800

    test

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,19 +1,56 @@
+import { log } from "console";
 import { NextRequest } from "next/server";
 
 export const OPENAI_URL = "api.openai.com";
+const CHAGLM_URL = process.env.CHAGLM_URL ?? "http://112.4.97.55:8001";
+
 const DEFAULT_PROTOCOL = "https";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 
-export async function requestOpenai(req: NextRequest) {
-  const controller = new AbortController();
-  const authValue = req.headers.get("Authorization") ?? "";
-  const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
-    "/api/openai/",
-    "",
-  );
+//key = 'xwxxxxx' or key = 'sk-xxxx' 进入openai
+//key 随意 用chatglm
+function getValues(k: string, p: string): [string, number] {
+  let provider = "openai";
+  let url = "";
+  let key = "" + k;
+  let path = "";
 
-  let baseUrl = BASE_URL;
+  if (k == "" || k.endsWith("chatglm")) {
+    provider = "chatglm";
+    key = "Bearer " + process.env.CHAGLM_TOKEN;
+  }
+
+  if (k.startsWith("Bearer " + process.env.TEST_PREFIX)) {
+    key = "Bearer " + process.env.OPENAI_API_KEY_TEST;
+  }
+
+  switch (provider) {
+    case "openai":
+      url = BASE_URL;
+      path = p.replaceAll("/api/openai/", "");
+      break;
+    case "chatglm":
+      url = CHAGLM_URL;
+      path = "v1/chat/completions";
+      break;
+  }
+
+  return [url, key, path];
+}
+
+export async function requestOpenai(req: NextRequest) {
+  const [url, key, path] = getValues(
+    req.headers.get("Authorization"),
+    `${req.nextUrl.pathname}${req.nextUrl.search}`,
+  );
+  console.log("[Test]", url, key, path);
+
+  const controller = new AbortController();
+  const authValue = key;
+  const openaiPath = path;
+
+  let baseUrl = url;
 
   if (!baseUrl.startsWith("http")) {
     baseUrl = `${PROTOCOL}://${baseUrl}`;
